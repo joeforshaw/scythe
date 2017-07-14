cf583ca1e08d89c8f6a0544f978eacea69c31edd
Joe Forshaw <joe.d.forshaw@gmail.com>
2017-07-14 18:27:27 +0100

    Enumerated the board

---
+++ src/enums/territory_side.js
@@ -0,0 +1,6 @@
+export const TOP_RIGHT    = 0;
+export const RIGHT        = 1;
+export const BOTTOM_RIGHT = 2;
+export const BOTTOM_LEFT  = 3;
+export const LEFT         = 4;
+export const TOP_LEFT     = 5;
--- src/models/board.js
+++ src/models/board.js
@@ -6,16 +6,88 @@ export default class Board {
   
   constructor() {
     this.territoryCoordinates = [
-      [ { x: 1, y: 0, type: Territories.BASE }, {x: 4, y: 0, type: Territories.BASE } ],
-      [ {x:1,y:1}, {x:2,y:1}, {x:3,y:1}, {x:4,y:1}, {x:5,y:1}, {x:6,y:1} ],
-      [ {x:0,y:2}, {x:1,y:2}, {x:2,y:2}, {x:3,y:2}, {x:4,y:2}, {x:5,y:2}, {x:6,y:2} ],
-      [ {x:0,y:3}, {x:1,y:3}, {x:2,y:3}, {x:3,y:3}, {x:4,y:3}, {x:5,y:3}, {x:6,y:3}, {x:7,y:3} ],
-      [ {x:0,y:4}, {x:1,y:4}, {x:4,y:4}, {x:3,y:4}, {x:4,y:4}, {x:5,y:4}, {x:6,y:4} ],
-      [ {x:0,y:5}, {x:1,y:5}, {x:2,y:5}, {x:3,y:5}, {x:4,y:5}, {x:5,y:5}, {x:6,y:5} ],
-      [ {x:0,y:6}, {x:1,y:6}, {x:4,y:6}, {x:3,y:6}, {x:4,y:6}, {x:5,y:6}, {x:6,y:6} ],
-      [ {x:0,y:7}, {x:1,y:7}, {x:2,y:7}, {x:3,y:7}, {x:4,y:7}, {x:5,y:7}, {x:6,y:7}, {x:7,y:7} ],
-      [ {x:2,y:8}, {x:3,y:8} ],
-    ];
+      // Row 1
+      [
+        { x: 1, y: 0, type: Territories.BASE, faction: Factions.ALBION },
+        { x: 4, y: 0, type: Territories.BASE, faction: Factions.NORDIC }
+      ],
+      // Row 2
+      [
+        { x: 1, y: 1, type: Territories.MOUNTAIN },
+        { x: 2, y: 1, type: Territories.FARM },
+        { x: 3, y: 1, type: Territories.VILLAGE },
+        { x: 4, y: 1, type: Territories.FOREST },
+        { x: 5, y: 1, type: Territories.TUNDRA },
+        { x: 6, y: 1, type: Territories.VILLAGE }
+      ],
+      // Row 3
+      [
+        { x: 0, y: 2, type: Territories.LAKE },
+        { x: 1, y: 2, type: Territories.TUNDRA },
+        { x: 2, y: 2, type: Territories.LAKE },
+        { x: 3, y: 2, type: Territories.TUNDRA, tunnel: true },
+        { x: 4, y: 2, type: Territories.MOUNTAIN },
+        { x: 5, y: 2, type: Territories.FARM },
+        { x: 6, y: 2, type: Territories.FARM }
+      ],
+      // Row 4
+      [
+        { x: 0, y: 3, type: Territories.BASE, faction: Factions.POLANIA },
+        { x: 1, y: 3, type: Territories.FOREST },
+        { x: 2, y: 3, type: Territories.MOUNTAIN, tunnel: true },
+        { x: 3, y: 3, type: Territories.FOREST },
+        { x: 4, y: 3, type: Territories.LAKE },
+        { x: 5, y: 3, type: Territories.FOREST, tunnel: true },
+        { x: 6, y: 3, type: Territories.VILLAGE },
+        { x: 7, y: 3, type: Territories.BASE, faction: Factions.RUSVIET }
+      ],
+      // Row 5
+      [
+        { x: 0, y: 4, type: Territories.FARM },
+        { x: 1, y: 4, type: Territories.VILLAGE },
+        { x: 4, y: 4, type: Territories.LAKE },
+        { x: 3, y: 4, type: Territories.FACTORY },
+        { x: 4, y: 4, type: Territories.MOUNTAIN },
+        { x: 5, y: 4, type: Territories.TUNDRA },
+        { x: 6, y: 4, type: Territories.MOUNTAIN }
+      ],
+      // Row 6
+      [
+        { x: 0, y: 5, type: Territories.FOREST },
+        { x: 1, y: 5, type: Territories.FOREST },
+        { x: 2, y: 5, type: Territories.FARM, tunnel: true },
+        { x: 3, y: 5, type: Territories.TUNDRA },
+        { x: 4, y: 5, type: Territories.LAKE },
+        { x: 5, y: 5, type: Territories.VILLAGE, tunnel: true },
+        { x: 6, y: 5, type: Territories.LAKE }
+      ],
+      // Row 7
+      [
+        { x: 0, y: 6, type: Territories.MOUNTAIN },
+        { x: 1, y: 6, type: Territories.VILLAGE },
+        { x: 4, y: 6, type: Territories.VILLAGE },
+        { x: 3, y: 6, type: Territories.TUNDRA, tunnel: true },
+        { x: 4, y: 6, type: Territories.FOREST },
+        { x: 5, y: 6, type: Territories.MOUNTAIN },
+        { x: 6, y: 6, type: Territories.TUNDRA }
+      ],
+      // Row 8
+      [
+        { x: 0, y: 7, type: Territories.BASE, faction: Factions.SAXONY },
+        { x: 1, y: 7, type: Territories.TUNDRA },
+        { x: 2, y: 7, type: Territories.LAKE },
+        { x: 3, y: 7, type: Territories.FARM },
+        { x: 4, y: 7, type: Territories.MOUNTAIN },
+        { x: 5, y: 7, type: Territories.VILLAGE },
+        { x: 6, y: 7, type: Territories.FARM },
+        { x: 7, y: 7, type: Territories.BASE, faction: Factions.TOGAWA }
+      ],
+      // Row 9
+      [
+        { x: 2, y: 8, type: Territories.BASE, faction: Factions.CRIMEA },
+        { x: 3, y: 8, type: Territories.VILLAGE }
+      ]
+    ]
   }
 
   render() {
