63eb009af0dad6452534b707f7eb9f711cbd0ed7
Joe Forshaw <joe.d.forshaw@gmail.com>
2017-07-22 10:48:54 +0100

    Added worker river move rule

--- src/models/worker.js
+++ src/models/worker.js
@@ -3,6 +3,7 @@ import WorkerRenderer from 'renderers/worker';
 import PubSub from 'pubsub-js';
 import { WORKER } from 'enums/units';
 import { CREATE_UNIT } from 'enums/topics';
+import { NORDIC } from 'enums/factions';
 import * as Territories from 'enums/territories';
 
 export default class Worker extends Unit {
@@ -10,18 +11,33 @@ export default class Worker extends Unit {
   constructor(state) {
     super({ renderer: WorkerRenderer, state: state });
     PubSub.publish(CREATE_UNIT, { type: WORKER, unit: this });
-
-    // Can't move onto lakes
-    this.addMoveRule(function(params) {
-      return params.territoryState.type != Territories.LAKE;
-    });
-
-    // Can't move onto territories controlled by another player
-    this.addMoveRule(function(params) {
-      return true;
-    });
-
-    // Only nordic workers can cross rivers
+    addMoveRules(this);
   }
 
+}
+
+function addMoveRules(worker) {
+  
+  // Can't move onto lakes
+  worker.addMoveRule(function(params) {
+    return params.territoryState.type != Territories.LAKE;
+  });
+
+  // Can't move onto territories controlled by another player
+  worker.addMoveRule(function(params) {
+    for (let id in params.territoryState.units) {
+      const otherUnitState = params.territoryState.units[id].getState();
+      if (params.unitState.faction != otherUnitState.faction) { return false; }
+    }
+    return true;
+  });
+
+  // Only nordic workers can cross rivers
+  worker.addMoveRule(function(params) {
+    const workerState = worker.getState();
+    if (workerState.faction == NORDIC) { return true; }
+    const currentTerritoryState = workerState.territory.getState();
+    if (!currentTerritoryState.rivers) { return true; }
+    return currentTerritoryState.rivers.indexOf(params.side) < 0; 
+  });
 }
